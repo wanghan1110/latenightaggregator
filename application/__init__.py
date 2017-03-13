f924fd787fc430aed0bc38b93fb0bc623a977a74
Jamesjue <canjian.myself@gmail.com>
2017-03-13 13:16:25 -0400

    fixed configuration files

--- application/__init__.py
+++ application/__init__.py
@@ -4,23 +4,26 @@ from flask_cache import Cache
 from flask_sqlalchemy import SQLAlchemy
 from aggregator import Aggregator
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
 app = Flask('application', instance_relative_config=True)
 
 # app.config.from_object('config.ProductionConfig')
 app.config.from_object('config.DevelopmentConfig')
-app.config.from_pyfile('application.cfg')
-app.config['SQLALCHEMY_DATABASE_URI']=app.config['SQLALCHEMY_DATABASE_URI_TEMPLATE'].format(app.config['SQLALCHEMY_DATABASE_USER'],
+try:
+    app.config.from_pyfile('application.cfg')
+    app.config['SQLALCHEMY_DATABASE_URI']=app.config['SQLALCHEMY_DATABASE_URI_TEMPLATE'].format(app.config['SQLALCHEMY_DATABASE_USER'],
                                                                                             app.config['SQLALCHEMY_DATABASE_PASSWORD'],
                                                                                             app.config['SQLALCHEMY_DATABASE_DB_NAME'])
+except IOError:
+    logger.info('no instance config foud')
 
 # db = SQLAlchemy(app)
 
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 cache.init_app(app)
-
 agg = Aggregator()
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 import views
